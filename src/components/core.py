e554c9ab0c66f017cd48a33dd270cfe76362a403
AvatanshuGupta <avatanshugupta@gmail.com>
2026-03-25 23:47:30 +0530

    core component done

---
+++ src/components/core.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_in,d_out,context_length,dropout,num_heads,qkv_biasing=False):
+        super().__init__()
+        assert (d_out % num_heads ==0), \
+        "d_out must be divisible by num_heads"
+
+        self.d_out=d_out
+        self.num_heads=num_heads
+
+        self.head_dim=d_out // num_heads
+
+        self.W_query=nn.Linear(d_in,d_out,bias=qkv_biasing)
+        self.W_key=nn.Linear(d_in,d_out,bias=qkv_biasing)
+        self.W_value=nn.Linear(d_in,d_out,bias=qkv_biasing)
+        self.out_proj=nn.Linear(d_out,d_out)
+        self.dropout=nn.Dropout(dropout)
+        self.register_buffer(
+                        "mask",
+                        torch.triu(torch.ones(context_length,context_length),diagonal=1)
+                             )
+        
+    def forward(self,x):
+        b,num_tokens,d_in=x.shape
+
+        keys=self.W_key(x)
+        values=self.W_value(x)
+        queries=self.W_query(x)
+
+        keys=keys.view(b,num_tokens,self.num_heads,self.head_dim)
+        values=values.view(b,num_tokens,self.num_heads,self.head_dim)
+        queries=queries.view(b,num_tokens,self.num_heads,self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores=queries @ keys.transpose(2,3)
+
+        mask_bool=self.mask.bool()[:num_tokens,:num_tokens]
+
+        attn_scores.masked_fill_(mask_bool,-torch.inf)
+
+        attn_weights=torch.softmax(attn_scores/keys.shape[-1]**0.5,dim=-1)
+        attn_weights=self.dropout(attn_weights)
+
+        context_vector=(attn_weights @ values).transpose(1,2)
+
+        context_vector=context_vector.contiguous().view(b,num_tokens,self.d_out)
+        context_vector=self.out_proj(context_vector)
+
+        return context_vector
+
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(self,embedding_dim):
+        super().__init__()
+        self.eps=1e-5
+        self.scale=nn.Parameter(torch.ones(embedding_dim))
+        self.shift=nn.Parameter(torch.zeros(embedding_dim))
+
+    def forward(self,x):
+        mean=x.mean(dim=-1,keepdim=True)
+        var=x.var(dim=-1,keepdim=True,unbiased=False)
+        norm_x=(x-mean)/torch.sqrt(var+self.eps)
+
+        return self.scale*norm_x + self.shift
+
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0/torch.pi))*( x + 0.044715 * torch.pow(x,3) )
+            ))
+    
+
+class FeedForward(nn.Module):
+    def __init__(self,cfg):
+        super().__init__()
+        self.layer=nn.Sequential(
+            nn.Linear(cfg['emb_dim'],4 * cfg['emb_dim'] ),
+            GELU(),
+            nn.Linear( 4 * cfg['emb_dim'],cfg['emb_dim'])
+            )
+        
+    def forward(self,x):
+        return self.layer(x)
